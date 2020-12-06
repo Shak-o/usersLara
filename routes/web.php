24ab808d90c2816e9f70424655f39f55dd8cda12
shalvaX <shalva.turashvili.1@btu.edu.ge>
2020-12-06 14:56:59 +0400

    bootstrap

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
